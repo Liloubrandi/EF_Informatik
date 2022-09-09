f78cae81a3c78ac9900506a4b3fe29fae97cb027
Lia <lia.brandenberger@gmx.ch>
2022-09-09 15:16:50 +0200

    5 zeilen möglichkeit

--- exercises/primzahlen.py
+++ exercises/primzahlen.py
@@ -1,14 +1,16 @@
-#primzahlen = []
-#rechnen = [2, 3, 5, 7]
-# for i in range(1, 100):
-#   if i > 1:
-#       for x in range(3):
-#           if (i % rechnen[x] == 0):
-#                break
-#       else:
-#           primzahlen.append(i)
-# print(primzahlen)
+# Möglichkeit 1:
+primzahlen = [2, 3, 5, 7]
+rechnen = [2, 3, 5, 7]
+for i in range(1, 100):
+    if i > 1:
+        for x in range(4):
+            if (i % rechnen[x] == 0):
+                break
+        else:
+            primzahlen.append(i)
+print(primzahlen)
 
+# Möglichkeit 2:
 primzahlen = []
 for i in range(1, 100):
     if i > 1:
@@ -17,4 +19,11 @@ for i in range(1, 100):
                 break
         else:
             primzahlen.append(i)
+print(primzahlen)
+
+# Möglichkeit 3:
+primzahlen = [2, 3, 5, 7]
+for i in range(2, 100):
+    if i % 2 != 0 and i % 3 != 0 and i % 5 != 0 and i % 7 != 0:
+        primzahlen.append(i)
 print(primzahlen)
