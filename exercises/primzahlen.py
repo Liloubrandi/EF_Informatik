51006e67386c97a150ba720b5d550eeec71e6cbf
Lia <lia.brandenberger@gmx.ch>
2022-09-09 13:36:48 +0200

    Primzahlen

--- exercises/primzahlen.py
+++ exercises/primzahlen.py
@@ -1,19 +1,19 @@
-primzahlen = []
-rechnen = [2, 3, 5, 7]
-for i in range(1, 100):
-    if i > 1:
-        for x in range(3):
-            if (i % rechnen[x] == 0):
-                break
-        else:
-            primzahlen.append(i)
-print(primzahlen)
+#primzahlen = []
+#rechnen = [2, 3, 5, 7]
+# for i in range(1, 100):
+#   if i > 1:
+#       for x in range(3):
+#           if (i % rechnen[x] == 0):
+#                break
+#       else:
+#           primzahlen.append(i)
+# print(primzahlen)
 
 primzahlen = []
 for i in range(1, 100):
     if i > 1:
         for x in range(2, i):
-            if (i % x == 0):
+            if (i % x) == 0:
                 break
         else:
             primzahlen.append(i)
