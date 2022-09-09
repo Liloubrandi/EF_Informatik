a1d683bd3a51339331c64acab84a2696bf57bde0
Lia <lia.brandenberger@gmx.ch>
2022-09-09 13:15:55 +0200

    primzahlen

--- exercises/primzahlen.py
+++ exercises/primzahlen.py
@@ -8,3 +8,13 @@ for i in range(1, 100):
         else:
             primzahlen.append(i)
 print(primzahlen)
+
+primzahlen = []
+for i in range(1, 100):
+    if i > 1:
+        for x in range(2, i):
+            if (i % x == 0):
+                break
+        else:
+            primzahlen.append(i)
+print(primzahlen)
