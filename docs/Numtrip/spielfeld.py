dea4c144135752b68f18f2cefee38093366f6f69
Lia <lia.brandenberger@gmx.ch>
2022-09-16 16:10:27 +0200

    spielfeld zufällig

--- docs/Numtrip/spielfeld.py
+++ docs/Numtrip/spielfeld.py
@@ -1,7 +1,23 @@
-spielfeld = [
-    [2, 4, 4, 8, 32]
-    [2, 2, 8, 16, 16]
-    [4, 8, 8, 2, 2]
-    [2, 4, 8, 16, 2]
-    [8, 8, 16, 2, 2]
-]
+import random
+# spielfeld = [
+#    [2, 4, 4, 8, 32],
+#    [2, 2, 8, 16, 16],
+#    [4, 8, 8, 2, 2],
+#    [2, 4, 8, 16, 2],
+#    [8, 8, 16, 2, 2],
+# ]
+
+spielfeld = []
+
+
+def pprint(feld):
+    for zeile in spielfeld:
+        print(zeile)
+
+
+for i in range(5):
+    spielfeld.append([])
+    for j in range(5):
+        spielfeld[i].append(2**random.randint(1, 4))
+
+pprint(spielfeld)
