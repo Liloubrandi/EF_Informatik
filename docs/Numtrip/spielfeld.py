55ab7260344e936d0fda7abb615a682994f8ba29
Lia <lia.brandenberger@gmx.ch>
2022-09-16 16:35:23 +0200

    spielfeld

--- docs/Numtrip/spielfeld.py
+++ docs/Numtrip/spielfeld.py
@@ -1,23 +1,30 @@
-import random
-# spielfeld = [
-#    [2, 4, 4, 8, 32],
-#    [2, 2, 8, 16, 16],
-#    [4, 8, 8, 2, 2],
-#    [2, 4, 8, 16, 2],
-#    [8, 8, 16, 2, 2],
-# ]
+#import random
 
-spielfeld = []
+#spielfeld = []
 
+# def pprint(feld):
+#    for zeile in spielfeld:
+#        print(zeile)
 
-def pprint(feld):
-    for zeile in spielfeld:
-        print(zeile)
+# for i in range(5):
+#    spielfeld.append([])
+#    for j in range(5):
+#        spielfeld[i].append(2**random.randint(1, 4))
 
+# pprint(spielfeld)
 
-for i in range(5):
-    spielfeld.append([])
-    for j in range(5):
-        spielfeld[i].append(2**random.randint(1, 4))
+spielfeld = [
+    [2, 4, 4, 8, 32],
+    [2, 2, 8, 16, 16],
+    [4, 8, 8, 2, 2],
+    [2, 4, 8, 16, 2],
+    [8, 8, 16, 2, 2],
+]
 
-pprint(spielfeld)
+for line in spielfeld:
+    print('+-----+-----+-----+-----+-----+')
+    print('|     |     |     |     |     |')
+    for field in line:
+        print(f'|  {field}  ', end='')
+    print('|')
+    print('|     |     |     |     |     |')
