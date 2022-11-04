dead16a3521a1261d9a5e9d53594a8b9ac345956
Lia <lia.brandenberger@gmx.ch>
2022-11-04 16:16:32 +0100

    bilder hangman

--- exercises/hangman.py
+++ exercises/hangman.py
@@ -12,6 +12,7 @@ falsch_geraten = []
 
 def show():
     print('Falsche Buchstaben:', falsch_geraten)
+    print(get_hangman(len(falsch_geraten)))
     for buchstabe in gesucht:
         if buchstabe in gefunden:
             print(buchstabe, end=' ')  # schreibt die erratenen Buchstaben
@@ -66,6 +67,30 @@ def play():
         print('gewonnen!:), das Wort war:', gesucht)
     else:
         print('verloren:(, gesucht wäre:', gesucht)
+
+
+HANGMANS_RAW = ''' 18 Zeichen ergeben ein Bild...
+                                         __________        __________        __________        __________        __________        __________        __________        __________        __________     
+                       |                 |                 |/                |/        |       |/        |       |/        |       |/        |       |/        |       |/        |       |/        |    
+                       |                 |                 |                 |                 |         O       |         O       |         O       |         O       |         O       |         O    
+                       |                 |                 |                 |                 |                 |         |       |        -|       |        -|       |        -|-      |        -|-   
+                       |                 |                 |                 |                 |                 |                 |                 |          \      |          \      |        / \   
+                       |                 |                 |                 |                 |                 |                 |                 |                 |                 |  GAME OVER   
+    _________         _|_______         _|_______         _|_______         _|_______         _|_______         _|_______         _|_______         _|_______         _|_______         _|_______     
+___/         \___ ___/         \___ ___/         \___ ___/         \___ ___/         \___ ___/         \___ ___/         \___ ___/         \___ ___/         \___ ___/         \___ ___/         \___ 
+'''.split('\n')[1:-1]  # ohne erste und letzte Zeile
+# schreibt jede Zeile einzeln auf.
+# enthält 11 ASCII-Bilder, für jeden Spiel-Zustand einen
+HANGMANS = ['\n'.join([line[i: i + 18] for line in HANGMANS_RAW])
+            for i in range(0, 11 * 18, 18)]
+# immer die ersten 18 Zeichen einer Zeile werden zu einem Bild zusammengetan.
+
+
+def get_hangman(nr):
+    '''
+    nr: int values from 0 to 10
+    '''
+    return HANGMANS[nr]
 
 
 play()
